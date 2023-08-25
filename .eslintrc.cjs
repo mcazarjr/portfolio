ffc5875b9bc3f76e8a936c3e104ebe2a6b485bcb
mcazarjr <meraldocazar@gmail.com>
2023-08-24 22:08:42 -0700

    Initial Commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,19 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/stylistic-type-checked'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
