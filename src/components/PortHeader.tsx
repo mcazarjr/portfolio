a068330943188d0e14702c0bb051be0a81729da7
mcazarjr <meraldocazar@gmail.com>
2023-08-28 23:23:50 -0700

    chore: install radix-ui-icons and create a hamburger menu with switching icon

--- src/components/PortHeader.tsx
+++ src/components/PortHeader.tsx
@@ -1,31 +1,45 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { HamburgerMenuIcon, Cross1Icon } from "@radix-ui/react-icons";
+
+// "underline text-l text-primary font-sans"
+// md:flex md:justify-center md:gap-8 md:py-4
+
 const PortHeader = () => {
+ const [menuFlag, setMenuFlag] = useState(false);
+
  return (
   <>
-   <header className="xl:container mx-auto">
-    <nav className="flex justify-center gap-8 py-4">
+   <header className="bg-accent xl:container mx-auto">
+    <button
+     onClick={() => setMenuFlag(!menuFlag)}
+     className="flex justify-center p-4"
+    >
+     {menuFlag ? <Cross1Icon /> : <HamburgerMenuIcon />}
+    </button>
+    <nav className={menuFlag ? "" : "hidden"}>
      <Link
       to="/"
-      className="underline text-l text-primary font-sans"
+      className=""
      >
       Home
      </Link>
      <Link
       to="/about"
-      className="underline text-l text-primary"
+      className=""
      >
       About
      </Link>
      <Link
       to="/projects"
-      className="underline text-l text-primary"
+      className=""
      >
       Projects
      </Link>
      <Link
       to="/contact"
-      className="underline text-l text-primary"
+      className=""
      >
       Contact
      </Link>
