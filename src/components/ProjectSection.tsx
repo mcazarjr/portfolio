4ed3982d42f0ce3d636357ea51a79ffe390c4cd6
mcazarjr <meraldocazar@gmail.com>
2023-09-03 15:33:45 -0700

    chore: fix portfolio page for mobile and desktop

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -8,9 +8,9 @@ const ProjectSection = forwardRef<HTMLElement, {}>((props, ref) => {
     <section
       id="projects"
       ref={ref}
-      className="2xl:container xl:w-11/12 xl:mx-auto py-8 md:py-16 px-4"
+      className="2xl:container xl:w-11/12 xl:mx-auto py-8 md:pt-24 md:pb-16 px-4 grid gap-4 md:gap-16"
     >
-      <div className="grid place-items-center pb-8">
+      <div className="grid place-items-center">
         <h2 className="uppercase font-bold font-mono text-xl flex gap-4 items-center text-primary lg:text-2xl">
           <span className="text-black">
             <Component1Icon />
@@ -22,35 +22,26 @@ const ProjectSection = forwardRef<HTMLElement, {}>((props, ref) => {
         </h2>
       </div>
 
-      <article className="p-4 rounded-lg shadow-md shadow-accent flex flex-col gap-4 md:relative">
-        <div className="border-b pb-4 md:border-b-0 md:pb-0 md:absolute md:top-4 md:right-4 md:w-7/12">
-          <img
-            src="images/NeatO_Desktop.png"
-            alt="NeatO Desktop"
-            className="md:rounded-lg md:pb-2 md:shadow-md"
-          />
-        </div>
-        <div className="flex flex-col gap-4 md:z-50 md:w-7/12 md:pt-10 md:pb-6">
+      <article className="px-4 py-8 rounded-lg flex flex-col gap-4 md:grid md:grid-cols-5">
+        <img
+          src="images/NeatO_Desktop.png"
+          alt="NeatO Desktop"
+          className="border-b pb-4 
+          md:rounded-lg md:pb-2 md:shadow-md md:shadow-accent md:border-b-0 md:col-start-[3] md:col-end-6 md:row-start-1 md:row-end-2"
+        />
+        <div
+          className="flex flex-col gap-4
+        md:pt-10 md:pb-6 md:row-start-1 md:row-end-2 md:col-start-1 md:col-end-4"
+        >
           <div className="">
-            <h3 className="text-secondary font-semibold font-mono">
+            <h3 className="text-primary font-semibold font-mono">
               Recent Project
             </h3>
             <h4 className="font-semibold font-mono text-2xl flex items-center gap-2">
               Neat-O
-              <a
-                href="https://neatoapp.live"
-                target="_blank"
-                className=""
-              >
-                <ExternalLinkIcon
-                  className="text-primary hover:text-secondary transition-all duration-200 ease-in-out"
-                  width="20"
-                  height="20"
-                />
-              </a>
             </h4>
           </div>
-          <p className="md:bg-secondary md:p-4 md:rounded-lg md:text-white">
+          <p className="md:bg-primary md:p-4 md:rounded-lg md:text-white md:max-w-xl">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
             error dignissimos quia quasi. Doloremque possimus sint pariatur
             quibusdam, atque quam excepturi, iure nisi voluptatibus magni unde
@@ -58,26 +49,26 @@ const ProjectSection = forwardRef<HTMLElement, {}>((props, ref) => {
             veritatis officia molestiae? Ipsum aspernatur ratione adipisci
             consequatur nostrum enim modi perspiciatis voluptas, sed impedit!
           </p>
-          <div className="flex gap-2 flex-wrap">
-            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm">
+          <div className="flex gap-2 flex-wrap md:max-w-xl">
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm bg-white">
               React
             </p>
-            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm">
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm bg-white">
               Redux
             </p>
-            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm">
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm bg-white">
               Node.js
             </p>
-            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm">
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm bg-white">
               Express.js
             </p>
-            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm">
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm bg-white">
               MongoDB
             </p>
-            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm">
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm bg-white">
               Mongoose
             </p>
-            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm">
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm bg-white">
               React Router Dom
             </p>
             <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm">
@@ -90,6 +81,92 @@ const ProjectSection = forwardRef<HTMLElement, {}>((props, ref) => {
               AWS
             </p>
           </div>
+          <a
+            href="https://neatoapp.live"
+            target="_blank"
+            className="group flex items-center gap-2 text-primary border border-primary w-fit rounded-lg p-2 hover:text-white hover:bg-primary transition-all duration-200 ease-in-out "
+          >
+            Live Demo
+            <ExternalLinkIcon
+              className="text-primary group-hover:text-white"
+              width="20"
+              height="20"
+            />
+          </a>
+        </div>
+      </article>
+
+      <article className="px-4 py-8 rounded-lg flex flex-col gap-4 md:grid md:grid-cols-5">
+        <img
+          src="images/NeatO_Desktop.png"
+          alt="NeatO Desktop"
+          className="border-b pb-4 
+          md:rounded-lg md:pb-2 md:shadow-md md:shadow-accent md:border-b-0 md:col-start-[1] md:col-end-4 md:row-start-1 md:row-end-2"
+        />
+        <div
+          className="flex flex-col gap-4
+        md:pt-10 md:pb-6 md:row-start-1 md:row-end-2 md:col-start-3 md:col-end-6 md:items-end"
+        >
+          <div className="">
+            <h3 className="text-primary font-semibold font-mono">
+              Recent Project
+            </h3>
+            <h4 className="font-semibold font-mono text-2xl flex items-center gap-2">
+              Neat-O
+            </h4>
+          </div>
+          <p className="md:bg-primary md:p-4 md:rounded-lg md:text-white md:max-w-xl">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
+            error dignissimos quia quasi. Doloremque possimus sint pariatur
+            quibusdam, atque quam excepturi, iure nisi voluptatibus magni unde
+            sed repudiandae omnis modi mollitia corrupti iusto magnam fugiat
+            veritatis officia molestiae? Ipsum aspernatur ratione adipisci
+            consequatur nostrum enim modi perspiciatis voluptas, sed impedit!
+          </p>
+          <div className="flex gap-2 flex-wrap md:max-w-xl md:justify-end">
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm bg-white">
+              React
+            </p>
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm bg-white">
+              Redux
+            </p>
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm bg-white">
+              Node.js
+            </p>
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm bg-white">
+              Express.js
+            </p>
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm bg-white">
+              MongoDB
+            </p>
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm bg-white">
+              Mongoose
+            </p>
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm bg-white">
+              React Router Dom
+            </p>
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm">
+              Tailwind
+            </p>
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm">
+              Socket.io
+            </p>
+            <p className="rounded-lg border px-2 border-accent text-slate-400 text-sm">
+              AWS
+            </p>
+          </div>
+          <a
+            href="https://neatoapp.live"
+            target="_blank"
+            className="group flex items-center gap-2 text-primary border border-primary w-fit rounded-lg p-2 hover:text-white hover:bg-primary transition-all duration-200 ease-in-out "
+          >
+            Live Demo
+            <ExternalLinkIcon
+              className="text-primary group-hover:text-white"
+              width="20"
+              height="20"
+            />
+          </a>
         </div>
       </article>
     </section>
